761e53885eb0c2b455fb0999ca848acdf6c5219d
Jeff Lyon <jeff@rubbingalcoholic.com>
2014-10-22 09:41:24 -0500

    warning about tweets

--- _includes/share_tweet_text.js
+++ _includes/share_tweet_text.js
@@ -1,1 +1,6 @@
+//
+//  WARNING! ~ If you need to use an apostrophe ==>'<==, precede it with a backslash
+//  So instead of "Bill O'Reilly" you would write "Bill O\'Reilly".
+//  If you fail to do this, everything will be ruined.
+//
 TWEET_TEXT = 'The FCC could save net neutrality or destroy it. I will #CallTheFCC and demand real #netneutrality https://www.callthefcc.com/'
